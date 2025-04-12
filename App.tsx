6af89c3ac18d823ca30ab7a179f9ffbd53c36e01
LeoAnders <leonardoanders15@gmail.com>
2025-04-12 16:52:46 -0300

    feat: utilzar `temas` e `fontes personalziadas`

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
-import { ThemeProvider } from "styled-components/native";
-import { Groups } from "./src/screens/Groups";
+import { ActivityIndicator } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import theme from "./src/theme";
+import theme from './src/theme';
+
+import { Groups } from '@screens/Groups';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
