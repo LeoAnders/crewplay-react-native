fae6b1b4aae5e4dff8d89c6d4c6956b0b7af3315
LeoAnders <leonardoanders15@gmail.com>
2025-04-14 22:20:09 -0300

    feat: criar componente de `List Empty`

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,13 +4,13 @@ import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 
 import { Container } from './styles';
 
 
 export function Groups() {
-  const [groups, setGroups] = useState(['Equipe 1', 'Equipe 2']);
-
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -27,6 +27,10 @@ export function Groups() {
           <GroupCard
             title={item}
           />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
     </Container>
