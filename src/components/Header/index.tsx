88581be8834c0875c372c2994b96523e1c8f1b6d
LeoAnders <leonardoanders15@gmail.com>
2025-05-20 21:02:48 -0300

    feat: implementa navegação com useNavigation e define tipagem global das rotas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from '@react-navigation/native';
+
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Container, Logo, BackIcon, BackButton } from "./styles";
 import logoImg from '@assets/logo.png';
@@ -7,11 +9,18 @@ type Props = {
 }
 
 export function Header({ showBackButton = false }: Props) {
+
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.navigate('groups');
+  }
+
   return (
     <SafeAreaView>
       <Container>
         {showBackButton &&
-          <BackButton>
+          <BackButton onPress={handleGoBack}>
             <BackIcon />
           </BackButton>
         }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
@@ -9,9 +10,14 @@ import { Button } from '@components/Button';
 
 import { Container } from './styles';
 
-
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
+
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate('new');
+  }
 
   return (
     <Container>
@@ -36,6 +42,7 @@ export function Groups() {
       />
       <Button
         title='Criar nova turma'
+        onPress={handleNewGroup}
       />
     </Container>
   );
