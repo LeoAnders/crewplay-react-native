45bb7c9d9db47fc57bd0714c8a345197218081ba
LeoAnders <leonardoanders15@gmail.com>
2025-05-20 21:08:30 -0300

    feat: aplica SafeAreaView no App para respeitar área segura da tela

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 import theme from './src/theme';
 
@@ -12,17 +11,13 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <SafeAreaProvider>
-      <ThemeProvider theme={theme}>
-        <SafeAreaView style={{ flex: 1 }}>
-          <StatusBar
-            barStyle="light-content"
-            backgroundColor="transparent"
-            translucent
-          />
-          {fontsLoaded ? <Routes /> : <Loading />}
-        </SafeAreaView>
-      </ThemeProvider>
-    </SafeAreaProvider>
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Routes /> : <Loading />}
+    </ThemeProvider>
   );
 }
