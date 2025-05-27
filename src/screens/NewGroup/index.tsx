130b4caf69fb1f9f5052775c0353b57897918d7d
LeoAnders <leonardoanders15@gmail.com>
2025-05-26 21:58:29 -0300

    feat(storage): adiciona tratamento de exceções e classe de erro personalizada

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,5 +1,7 @@
 import { useState, useCallback } from "react";
 import { Container, Content, Icon } from "./styles";
+import { Alert } from "react-native";
+import { AppError } from "@utils/AppError";
 
 import { groupCreate } from "@storage/group/groupCreate";
 
@@ -15,11 +17,20 @@ export function NewGroup() {
   const navigation = useNavigation();
 
   async function handleNew() {
+    if (group.trim().length === 0) {
+      return Alert.alert('Novo Grupo', 'Informe o nome da turma.');
+    }
+
     try {
       await groupCreate(group);
       navigation.navigate("players", { group });
     } catch (error) {
-      console.log(error);
+      if (error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message);
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.');
+        console.log(error);
+      }
     }
   }
 
